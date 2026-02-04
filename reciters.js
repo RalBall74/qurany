3d7ac915db2706e3edcc3612719aa77d20b9279c
Assem M Abu Alnasr <assem.abualnasr@gmail.com>
2026-02-04 19:45:51 +0200

    Add files via upload

---
+++ reciters.js
@@ -0,0 +1,29 @@
+const recitersData = [
+    {
+        id: "maher",
+        name: "ماهر المعيقلي",
+        img: "4.jpg",
+        server: "https://server12.mp3quran.net/maher/",
+        rewayah: "حفص عن عاصم"
+    },
+    {
+        id: "afasy",
+        name: "مشاري راشد العفاسي",
+        img: "2.jpg",
+        server: "https://server8.mp3quran.net/afs/",
+        rewayah: "حفص عن عاصم"
+    },
+    {
+        id: "dosari",
+        name: "ياسر الدوسري",
+        img: "3.jpg",
+        server: "https://server11.mp3quran.net/yasser/",
+        rewayah: "حفص عن عاصم"
+    }
+];
+
+// Surah list structure (Simplified, we will fetch full via API if needed)
+const surahListMeta = [
+    { id: 1, name: "الفاتحة", englishName: "Al-Fatihah" },
+    { id: 2, name: "البقرة", englishName: "Al-Baqarah" },
+];
